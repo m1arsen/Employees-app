6fcfb63bd904454e4f614314c9fcfbdc08a64e9d
m1arsen <m1arsen.web@gmail.com>
2023-02-11 09:20:40 +0300

    first commit

---
+++ src/components/employees-add-form/employees-add-form.js
@@ -0,0 +1,23 @@
+import './employees-add-form.css';
+
+const EmployeesAddForm = () => {
+    return (
+        <div className="app-add-form">
+            <h3>Добавьте нового сотрудника</h3>
+            <form
+                className="add-form d-flex">
+                <input type="text"
+                    className="new-post-label"
+                    placeholder="Как его зовут?" />
+                <input type="number"
+                    className="new-post-label"
+                    placeholder="З/П в $?" />
+
+                <button type="submit"
+                        className="btn btn-outline-light">Добавить</button>
+            </form>
+        </div>
+    )
+}
+
+export default EmployeesAddForm;
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,11 @@
+import './search-panel.css';
+
+const SearchPanel = () => {
+    return (
+        <input type="text"
+                className="search-input"
+                placeholder="Найти сотрудника"/>
+    )
+}
+
+export default SearchPanel;
